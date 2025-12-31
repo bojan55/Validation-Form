3811f3fd81b4b45eacd3c981714b864be99bd118
bojan55 <bojan.bajic55@gmail.com>
2025-12-31 18:06:30 +0100

    Create index.html, validator.js and script.js files.

---
+++ script.js
@@ -0,0 +1,13 @@
+let config = {
+    'imePrezime': {
+        required: true,
+        minLength: 3,
+        maxLength: 50
+    },
+    'korisnickoIme': {
+        required: true,
+        minLength: 5,
+        maxLength: 50
+    },
+    
+}
