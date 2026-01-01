36762b7d2f5ab71feb5e73b80324efc783095ce9
bojan55 <bojan.bajic55@gmail.com>
2026-01-01 16:47:03 +0100

    Create Validator class.

--- validator.js
+++ validator.js
@@ -1,6 +1,32 @@
 class Validator {
-    constructor(){
-        
+
+    constructor(config){
+        this.elementsConfig = config;
+        this.errors = {};
+        this.generateErrorsObject();
+        this.inputListener();
+    }
+
+    generateErrorsObject(){
+        for(let field in this.elementsConfig){
+            this.errors[field] = [];
+        }
+    }
+
+    inputListener(){
+        let inputSelector = this.elementsConfig;
+        for(let field in inputSelector){
+            let el = document.querySelector(`input[name="${field}"]`);
+
+            el.addEventListener('input', this.validate.bind(this));
+        }
+    }
+
+    validate(e){
+        let elFields = this.elementsConfig;
+        let field = e.target;
+        let fieldName = field.getAttribute('name');
+        let fieldValue = field.value;
     }
 
 }
